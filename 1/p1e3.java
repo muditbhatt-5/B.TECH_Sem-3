d3b7aabad83bbb72414112101aa4781d5cc6de51
Mudit Bhatt <154462822+muditbhatt-5@users.noreply.github.com>
2025-01-14 19:20:46 +0530

    Practical-1

---
+++ 1/p1e3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class p1e3
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter value of a through Scanner");
+		int a=sc.nextInt();
+		System.out.println("You entered value through Scanner is :"+a);
+	}
+}
